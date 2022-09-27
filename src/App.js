504ef1778fcb4e26d69bc48a6645061ade343901
Ayush Mishra <ayush@Ayushs-MacBook-Air.local>
2022-09-27 22:47:47 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [data, setData] = React.useState([]);
+    React.useEffect(() => {
+        const fetchLocation = async () => {
+            await fetch("https://jsonplaceholder.typicode.com/users")
+                .then((res) => res.json())
+                .then((data) => {
+                    setData(data);
+                    console.log(data);
+                });
+        };
+        fetchLocation();
+    }, []);
+
+    if (!data.length) return <div>Loading...</div>
+    return (
+        <div>
+            {
+                data.map((item, index) =>
+                    <div
+                        key={index}
+                        style={{
+                            marginBottom: '3rem',
+                            marginTop: '0.5rem',
+                            backgroundColor: 'yellow',
+                            padding: '1rem 2rem'
+                        }}>
+                        <p>NAME: {data[index].name}</p>
+                        <p>EMAIL: {data[index].email}</p>
+                        <p>PHONE: {data[index].phone}</p>
+                        <p>ADDRESS: {data[index].address.street}</p>
+                    </div>
+                )
+            }
+        </div>
+    );
 }
 
 export default App;
